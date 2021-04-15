1ae48a61bf887dc70a12cf0042a66c428c1d9f96
Noa Mourareau <mourareaun@eisti.eu>
2021-04-15 16:28:54 +0200

    Update audiodream.sql

--- sql/audiodream.sql
+++ sql/audiodream.sql
@@ -5,38 +5,43 @@ DROP TABLE Commande;
 DROP TABLE Appartenir;
 
 CREATE TABLE Categorie(
-    idCat INT PRIMARY KEY,
-    nom VARCHAR(20)
+    idCat INT NOT NULL AUTO_INCREMENT,
+    nom VARCHAR(20),
+    PRIMARY KEY(idCat)
 );
 
 CREATE TABLE Produit(
-    idProduit INT PRIMARY KEY,
+    idProduit INT NOT NULL AUTO_INCREMENT,
     nomP VARCHAR(20),  
     prix INT,
     img VARCHAR(20),
     stock INT,
     idCat INT,
+    PRIMARY KEY(idProduit),
     FOREIGN KEY fk_categorie(idCat) REFERENCES Categorie(idCat)
 );
 
 CREATE TABLE Utilisateur(
-    pseudo VARCHAR(25) PRIMARY KEY,
+    pseudo VARCHAR(25) NOT NULL,
     prenom VARCHAR(20),
     nom VARCHAR(20),  
     mail VARCHAR(100),
-    motDePasse VARCHAR(50)
+    motDePasse VARCHAR(50),
+    PRIMARY KEY(pseudo)
 );
 
 CREATE TABLE Commande(
-    idCommande INT PRIMARY KEY,
+    idCommande INT NOT NULL,
     pseudo VARCHAR(25),
+    PRIMARY KEY(idCommande),
     FOREIGN KEY fk_utilisateur(pseudo) REFERENCES Utilisateur(pseudo)
 );
 
 CREATE TABLE Appartenir(
-    idCommande INT PRIMARY KEY,
-    idProduit INT PRIMARY KEY,
+    idCommande INT NOT NULL,
+    idProduit INT NOT NULL,
     quantite INT,  
+    PRIMARY KEY(idCommande,idProduit),
     FOREIGN KEY fk_commande(idCommande) REFERENCES Commande(idCommande),
     FOREIGN KEY fk_produit(idProduit) REFERENCES Produit(idProduit)
 );
